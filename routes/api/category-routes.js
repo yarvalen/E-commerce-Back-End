33df79e676f4efa3a87114cc043ccbcb589e30dd
Blue Valenz <bluevalenz@Blues-MacBook-Pro.local>
2023-04-24 21:00:36 -0400

    fixed api

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,18 +3,46 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try{
+    const categoryData = await Category.findAll({
+      include: [{ model: Product}]
+    });
+    res.status(200).json(categoryData);
+  } catch (err){
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-});
+  router.get('/:id', async (req,res) => {
+    try{
+      const categoryData = await Category.findByPk(req.params.id, {
+        include: [{ model: Product}],
+      });
+      if (!categoryData) {
+        res.status(404).json({message:'No category found with this id!'});
+        return;
+      }
+      res.status(200).json(categoryData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
-router.post('/', (req, res) => {
-  // create a new category
+
+router.post('/', async (req, res) => {
+    // create a new category
+
+  try{
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
